e4568c87dae8ec16b2501407d28c5cfed8281e8c
johnyking <johnyking911@163.com>
2020-10-20 16:34:10 +0800

    版本1

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -6,5 +6,6 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HelloGit!!!");
+        System.out.println("版本1");
     }
 }
