7d8d7c51c9309963a82bdf76bc11891721a43e14
johnyking <johnyking911@163.com>
2020-10-20 16:30:23 +0800

    HelloGit commit

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+/**
+ * Created by jinyue on 2020/10/20.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit!!!");
+    }
+}
