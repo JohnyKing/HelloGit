ac4dad5ea22ee406a936b31b985f49023145d797
悦跃阅 <34095332+JohnyKing@users.noreply.github.com>
2020-10-20 16:59:14 +0800

    本地库拉入测试
    追加一行代码测试

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -10,5 +10,6 @@ public class HelloGit {
         System.out.println("分支添加");
         System.out.println("主干在第11行添加代码");
         System.out.println("分支在第11行添加代码");
+        System.out.println("鄙人在线修改了程序代码");
     }
 }
